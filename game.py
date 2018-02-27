7c79525027f29987a5cc1ac8e348dd2c60387779
Chris Burgess <burgesschristopherr@gmail.com>
2018-02-27 13:06:54 -0500

    Ball moves
    The ball moves

--- game.py
+++ game.py
@@ -1,12 +1,25 @@
 import pygame, sys
-from pygame.locals import *
-
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption("Yo World, Sup Guy")
-while True:
+
+size = width, height = 400, 300
+speed = [5, 5]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("ball.png")
+ballrect = ball.get_rect()
+
+while 1:
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
